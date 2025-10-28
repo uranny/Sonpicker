c9109ecd5ed5e96f96ee01c72545965469422001
uranny <164876325+uranny@users.noreply.github.com>
2025-10-28 10:40:07 +0900

    feat : create SignCard component #1

---
+++ src/components/signcard/index.tsx
@@ -0,0 +1,36 @@
+
+export type SignCardProps = {
+    id : number
+    imgUrl: string;
+    label: string;
+    onClick ?: () => void
+};
+
+function SignCard({id, imgUrl, label, onClick }: SignCardProps) {
+
+    return (
+        <button key={id}
+            className={`
+                flex flex-col
+                bg-white    
+                pt-2 pr-2 pl-2 pb-2
+                rounded-2xl
+                border-slate-200
+                border-3
+                gap-1
+                cursor-pointer
+            `}
+            onClick={onClick}
+        >
+            <img
+                className="w-24 m-auto rounded-2xl"
+                style={{ boxShadow: "inset 0 4px 6px rgba(0,0,0,0.3)" }}
+                src={imgUrl}
+                alt={label}
+            />
+            <p className="text-center text-2xl font-black text-black">{label}</p>
+        </button>
+    );
+}
+
+export default SignCard;
